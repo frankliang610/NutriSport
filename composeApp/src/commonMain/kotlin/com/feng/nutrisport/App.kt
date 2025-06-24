80aa7aaa5f58f87be14e42168d42f76c66467966
Feng Liang <liangfeng610@gmail.com>
2025-06-24 22:27:28 +1200

    add home screen nested navitation bottom bar

--- composeApp/src/commonMain/kotlin/com/feng/nutrisport/App.kt
+++ composeApp/src/commonMain/kotlin/com/feng/nutrisport/App.kt
@@ -1,28 +1,18 @@
 package com.feng.nutrisport
 
 import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.safeContentPadding
-import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import com.mmk.kmpauth.google.GoogleAuthCredentials
 import com.mmk.kmpauth.google.GoogleAuthProvider
 import com.nutrisport.data.domain.CustomerRepository
-import com.nutrisport.navigation.Screen
+import com.nutrisport.shared.navigation.Screen
 import com.nutrisport.navigation.SetupNavGraph
 import com.nutrisport.shared.Constants
-import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
-import nutrisport.composeapp.generated.resources.Res
-import nutrisport.composeapp.generated.resources.compose_multiplatform
 import org.koin.compose.koinInject
 
 @Composable
