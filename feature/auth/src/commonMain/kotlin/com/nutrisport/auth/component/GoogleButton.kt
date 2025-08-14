969fca7fae68d5668b9aaa259da8df07acab4a3d
Feng Liang <liangfeng610@gmail.com>
2025-08-14 22:53:38 +1200

    add a CustomTextField in the profile component

--- feature/auth/src/commonMain/kotlin/com/nutrisport/auth/component/GoogleButton.kt
+++ feature/auth/src/commonMain/kotlin/com/nutrisport/auth/component/GoogleButton.kt
@@ -29,6 +29,7 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.unit.dp
+import com.nutrisport.shared.BorderIdle
 import com.nutrisport.shared.FontSize
 import com.nutrisport.shared.IconSecondary
 import com.nutrisport.shared.Resources
@@ -47,7 +48,7 @@ fun GoogleButton(
     icon: DrawableResource = Resources.Image.GoogleLogo,
     shape: Shape = RoundedCornerShape(size = 99.dp),
     backgroundColor: Color = SurfaceLighter,
-    borderColor: Color = SurfaceDarker,
+    borderColor: Color = BorderIdle,
     progressIndicatorColor: Color = IconSecondary,
     onClick: () -> Unit,
 ) {
